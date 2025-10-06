a7886685f86f0e85b1e138eef327fc49cac487f9
Szapt <santiagod.zapata@udea.edu.co>
2025-10-05 21:48:36 -0500

    Now

--- platzi-play/src/main/java/com/platzi/platzi_play/HelloController.java
+++ platzi-play/src/main/java/com/platzi/platzi_play/HelloController.java
@@ -1,4 +1,14 @@
 package com.platzi.platzi_play;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello World";
+    }
 }
